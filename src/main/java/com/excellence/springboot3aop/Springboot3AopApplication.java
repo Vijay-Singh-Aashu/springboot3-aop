ea65af08b7fbbe687dc76c9c1cbf140588157f48
Vijay Singh <vijaysinghaashu@gmail.com>
2023-11-28 11:07:03 +0530

    springboot-aop

---
+++ src/main/java/com/excellence/springboot3aop/Springboot3AopApplication.java
@@ -0,0 +1,13 @@
+package com.excellence.springboot3aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3AopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3AopApplication.class, args);
+	}
+
+}
